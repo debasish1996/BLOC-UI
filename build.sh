b406775f7b7a2f87e08d8feb7a4414a8de83ac47
Debasish Sahoo <debasish.sahoo@meetingdecisions.com>
2026-03-26 11:06:55 +0530

    feat: streamline build process by consolidating build steps in build.sh

--- build.sh
+++ build.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
+CONFIG=""
+if [[ "$1" == "--production" ]]; then
+  CONFIG="--configuration production"
+fi
+
 echo "Building all projects..."
 
 echo "Building bloc-ui-core..."
-ng build bloc-ui-core
+ng build bloc-ui-core $CONFIG
 
 echo "Building bloc-ui-theme..."
-ng build bloc-ui-theme
+ng build bloc-ui-theme $CONFIG
 
 echo "Building bloc-ui-modal..."
-ng build bloc-ui-modal
+ng build bloc-ui-modal $CONFIG
 
 echo "Building bloc-ui-table..."
-ng build bloc-ui-table
+ng build bloc-ui-table $CONFIG
 
 echo "Building bloc-ui-toast..."
-ng build bloc-ui-toast
+ng build bloc-ui-toast $CONFIG
 
 echo "Building bloc-ui-date-picker..."
-ng build bloc-ui-date-picker
+ng build bloc-ui-date-picker $CONFIG
 
 echo "Building bloc-ui-tab..."
-ng build bloc-ui-tab
+ng build bloc-ui-tab $CONFIG
 
 echo "Building bloc-ui..."
-ng build bloc-ui
+ng build bloc-ui $CONFIG
 
 echo "Building demo..."
-ng build demo
+ng build demo $CONFIG
 
 echo "All projects built successfully!"
