890930ac9aede542169156e84c687d20dcf41f4d
deba <debasishsahoo071@gmail.com>
2026-04-11 14:28:36 +0530

    feat: update launch configuration to use input variables for URLs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const appHost = 'localhost:4200';
+const appUrl = `http://${appHost}`;
+
 export default defineConfig({
     testDir: './e2e',
     fullyParallel: true,
@@ -8,13 +11,13 @@ export default defineConfig({
     workers: process.env['CI'] ? 1 : undefined,
     reporter: [['html', { outputFolder: 'playwright-report' }]],
     use: {
-        baseURL: 'http://localhost:4200',
+        baseURL: appUrl,
         trace: 'on-first-retry',
     },
     projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
     webServer: {
         command: 'npm start',
-        url: 'http://localhost:4200',
+        url: appUrl,
         reuseExistingServer: !process.env['CI'],
         timeout: 120_000,
     },
